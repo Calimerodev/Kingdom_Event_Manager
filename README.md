3c2567a9ff46bd3d6b00a85de1f9b303aca7b04c
Christian-Alexis-Sierra-Cruz <christiandmoj6@gmail.com>
2025-10-12 14:04:32 -0400

    Idea 1

---
+++ README.md
@@ -0,0 +1,7 @@
+En un reino muy lejano en europa es el anio 100 AC y eres el Rey y
+gobiernas tu reino y las aldeas de los alrededores y tu como Rey
+debes administrar todos los eventos que ocurren lo recursos del
+reino el dinero los alimentos la fuerza de trabajo y los impuestos
+de los aldeanos y debes saber en todo momento los recursos que tienes
+y los evetos que se estan sucediendo en tiempo real y por supueesto los
+que no han sucedido todavia pero en algun momento sucederan
--- models/models.py
+++ models/models.py
@@ -1,5 +1,32 @@
 import sqlite3
 
+class Depend_Resources():
+    pass
+
+
+class Incompatibles_Resources():
+    def __init__(self):
+        self.connection = sqlite3.Connection("DATA BASE.db")
+        self.cursor = self.connection.cursor()
+
+
+    def create(self):
+        sql = """
+            CREATE TABLE Incmpatibles_Rsources (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_resource1 INTEGER,
+                id_resource2 INTEGER
+            )
+        """
+
+        self.cursor.execute(sql)
+        self.connection.commit()
+
+
+    def insert():
+        pass
+
+
 class Events_Resources_Relation():
     def __init__(self):
         self.connection = sqlite3.Connection("DATA BASE.db")
@@ -24,7 +51,7 @@ class Events_Resources_Relation():
             INSERT INTO Events_Resources_Relation(id, id_event, id_resource) VALUES (
                 NULL,
                 {id_event},
-                {id_resource} 
+                {id_resource}
             )
         """
 
@@ -56,9 +83,30 @@ class Events_Resources_Relation():
         """
         self.cursor.execute(sql , (f"{id_relation}",))
         self.connection.commit()
+    
+
+    def update(
+            self,
+            new_id_event=None,
+            new_id_resource=None,
+            id_relation = None
+        ):
+        
+        change_id_event = "" if new_id_event == None else f"id_event = {new_id_event}"
+        change_id_resource = "" if new_id_resource == None else f",id_resource = {new_id_resource}"
+
+        sql = f"""
+            UPDATE Events_Resources_Relation SET
+                {change_id_event}
+                {change_id_resource}
+            WHERE id = ?
+        """
+
+        self.cursor.execute(sql, (id_relation,))
+        self.connection.commit()
 
 
-class Resources():
+class Resources(Events_Resources_Relation):
     def __init__(self):
         self.connection = sqlite3.connect("DATA BASE.db")
         self.cursor = self.connection.cursor()
@@ -97,7 +145,13 @@ class Resources():
         return self.cursor.fetchall()
 
 
-    def insert(self , name_resource , resource_cuantity , resource_aviable, assigned_to):
+    def insert(self,
+               name_resource,
+               resource_cuantity,
+               resource_aviable, 
+               assigned_to
+            ):
+     
         sql = f"""
             INSERT INTO Resources(id,name,resources_cuantity,resources_aviable,assigned_to) VALUES (
                 NULL,
@@ -121,8 +175,36 @@ class Resources():
         self.connection.commit()
 
 
-class Events():
+    def update(
+            self,
+            new_name_resource = None,
+            new_cuantity_resource=None,
+            new_aviable_resource=None,
+            new_assigned_to=None,
+            id_resource = None
+        ):
+
+        change_name = "" if new_name_resource == None else f"name = '{new_name_resource}'"
+        change_cuantity_resources = "" if new_cuantity_resource == None else f",resources_cuantity = {new_cuantity_resource}"
+        change_aviable_resources = "" if new_aviable_resource == None else f",resources_aviable = {new_aviable_resource}"
+        change_assigned_to = "" if new_assigned_to == None else f",assigned_to = '{new_assigned_to}'"
+        
+        sql = f"""
+            UPDATE Resources SET
+                {change_name}
+                {change_cuantity_resources}
+                {change_aviable_resources}
+                {change_assigned_to}
+            WHERE id = ?
+        """
+
+        self.cursor.execute(sql , (id_resource,))
+        self.connection.commit()
+
+
+class Events(Events_Resources_Relation):
     def __init__(self):
+        super().__init__()
         self.connection = sqlite3.Connection("DATA BASE.db")
         self.cursor = self.connection.cursor()
 
@@ -151,18 +233,26 @@ class Events():
         return self.cursor.fetchall()
 
 
-    def get_element(self , id_event):
+    def get_element(self , id_event , id_resource):
         
         all_events = self.getall()
         
         for event in all_events:
-            if event[0] == id_event:
+            if event[1] == id_event and event[2] == id_resource:
                 return event
             
         return None
 
 
-    def insert(self , name_event , type_event , resources_event , start_event , end_event):
+    def insert(
+            self,
+            name_event,
+            type_event,
+            resources_event,
+            start_event,
+            end_event
+            ):
+        
         sql = f"""
             INSERT INTO Events(id,name,place,resources,start,end) VALUES (
                 NULL,
@@ -186,13 +276,33 @@ class Events():
         self.connection.commit()
 
 
-data = Events()
-data2 = Resources()
+    def update(
+            self,
+            new_name_event = None,
+            new_type_event=None,
+            new_resource_event=None,
+            new_start_event=None,
+            new_end_event=None,
+            id_event = None
+        ):
+        
+        change_name = "" if new_name_event == None else f"name = '{new_name_event}'"
+        change_type = "" if new_type_event == None else f",place = {new_type_event}"
+        change_resource = "" if new_resource_event == None else f",resources = '{new_resource_event}'"
+        change_start = "" if new_start_event == None else f",start = '{new_start_event}'"
+        change_end = "" if new_end_event == None else f",end = '{new_end_event}'"
+        
+        sql = f"""
+            UPDATE Events SET 
+                {change_name}
+                {change_type}
+                {change_resource}
+                {change_start}
+                {change_end}
+            WHERE id = ? 
+        """
+        self.cursor.execute(sql , (id_event,))
+        self.connection.commit()
 
-data.create()
-data2.create()
 
-#data.create()
-
-print(data.getall())
-
+data = Incompatibles_Resources()
